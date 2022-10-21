62176f78d9150f47199aa02a03c44a7c31197486
Fabien Spindler <Fabien.Spindler@inria.fr>
2022-10-21 10:30:56 +0200

    Initial commit

---
+++ test-scandir.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include <sys/types.h>
+#include <dirent.h>
+
+std::vector<std::string> getDirFiles(const std::string &pathname)
+{
+  std::vector<std::string> files;
+  struct dirent **list = NULL;
+  std::string dirName = pathname;
+  int filesCount = scandir(dirName.c_str(), &list, NULL, NULL);
+  std::cout << "filesCount: " << filesCount << std::endl;
+
+  if (filesCount == -1) {
+    std::cout<< "Cannot read files of directory: " << dirName << std::endl;;
+  }
+  for (int i = 0; i < filesCount; i++) {
+    std::string fileName = list[i]->d_name;
+    std::cout << "Found file: " << fileName << std::endl;
+    if (fileName != "." && fileName != "..") {
+      files.push_back(fileName);
+    }
+    free(list[i]);
+  }
+  free(list);
+  std::sort(files.begin(), files.end());
+  return files;
+}
+
+int main()
+{
+  std::vector<std::string> files;
+  files = getDirFiles("/tmp/unknown/video");
+  std::cout << "The folder contains:" << std::endl;
+  for (int i = 0; i < files.size(); i++) {
+    std::cout << files[i] << std::endl;
+  }
+  std::cout << "The end" << std::endl;
+  return EXIT_SUCCESS;
+}
